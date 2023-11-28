e5dbdb6dfc735e480aa20225d1837e2d237629bc
Huan Bui <huuuhuannt1998@gmail.com>
2023-11-28 13:52:52 -0500

    update pref handler

--- smartthingsCLI/zigbee-motion-sensor/src/init.lua
+++ smartthingsCLI/zigbee-motion-sensor/src/init.lua
@@ -31,20 +31,81 @@ local battery_table = {
   [1.50] = 0
 }
 
-local function check_interval(driver, device, event, args)
-  if (device.preferences.minReportingInterval < 30)  && (device.preferences.minReportingInterval > 3600) then
-    device:send("Error test")
+-- local function interval_check_handler(driver, device)
+--   local minInterval = device.preferences.minReportingInterval
+--   local maxInterval = device.preferences.maxReportingInterval
+
+--   if minInterval < 30 or maxInterval > 3600 then
+--     -- Replace the following line with your desired error handling approach
+--     device:send("Reporting interval is out of acceptable range")
+--   end
+-- end
+
+-- -- Function to handle changes in preferences
+-- local function preferences_changed_handler(driver, device, event, args)
+--   if args.old_st_store.preferences.minReportingInterval ~= device.preferences.minReportingInterval then
+--     driver:log("minReportingInterval changed to: " .. tostring(device.preferences.minReportingInterval))
+--     interval_check_handler(driver, device)  -- Call interval check here
+--   end
+
+--   if args.old_st_store.preferences.maxReportingInterval ~= device.preferences.maxReportingInterval then
+--     driver:log("maxReportingInterval changed to: " .. tostring(device.preferences.maxReportingInterval))
+--     interval_check_handler(driver, device)  -- Call interval check here
+--   end
+-- end
+
+-- local function init_handler(driver, device)
+--   battery_defaults.enable_battery_voltage_table(device, battery_table)
+-- end
+
+-- local smartthings_motion = {
+--   NAME = "SmartThings Motion Sensor",
+--   lifecycle_handlers = {
+--     init = init_handler,
+--     check_interval = interval_check_handler,
+--     preferences_changed = preferences_changed_handler
+--   },
+--   can_handle = function(opts, driver, device, ...)
+--     return device:get_manufacturer() == "SmartThings"
+--   end
+-- }
+
+-- Handler to check reporting intervals
+local function interval_check_handler(driver, device)
+  local minInterval = device.preferences.minReportingInterval
+  local maxInterval = device.preferences.maxReportingInterval
+
+  if minInterval < 30 or maxInterval > 3600 then
+    driver:log("Error: Reporting interval is out of acceptable range")
+    -- You can add additional error handling here if needed
   end
 end
 
-local function init_handler(driver, device)
-  battery_defaults.enable_battery_voltage_table(device, battery_table)
+-- Function to handle changes in preferences
+local function preferences_changed_handler(driver, device, event, args)
+  if args.old_st_store.preferences.minReportingInterval ~= device.preferences.minReportingInterval then
+    driver:log("minReportingInterval changed to: " .. tostring(device.preferences.minReportingInterval))
+    interval_check_handler(driver, device)  -- Call interval check here
+  end
+
+  if args.old_st_store.preferences.maxReportingInterval ~= device.preferences.maxReportingInterval then
+    driver:log("maxReportingInterval changed to: " .. tostring(device.preferences.maxReportingInterval))
+    interval_check_handler(driver, device)  -- Call interval check here
+  end
 end
 
+-- Initialization handler
+local function init_handler(driver, device)
+  battery_defaults.enable_battery_voltage_table(device, battery_table)
+  interval_check_handler(driver, device)  -- Call interval check during initialization
+end
+
+-- Main device handler
 local smartthings_motion = {
   NAME = "SmartThings Motion Sensor",
   lifecycle_handlers = {
-    init = init_handler
+    init = init_handler,
+    infoChanged = preferences_changed_handler
   },
   can_handle = function(opts, driver, device, ...)
     return device:get_manufacturer() == "SmartThings"
